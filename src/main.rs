4fc1e40766e41a2ff6d50635f7d65f3b19a478a2
Niclas Hoyer <info@niclashoyer.de>
2022-01-26 00:43:31 +0100

    Workaround for RUSTSEC-2020-0159

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ use std::sync::{Arc, Mutex};
 use clap::Parser;
 #[cfg(feature = "systemd")]
 use listenfd::ListenFd;
-use slog::Drain;
-use slog::{debug, info, o};
+use tracing::{debug, info};
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -42,10 +41,7 @@ struct Args {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-    let log = slog::Logger::root(drain, o!());
+    tracing_subscriber::fmt::init();
 
     let args = Args::parse();
 
@@ -81,13 +77,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     })?;
 
+    info!("waiting for connection…");
     let (stream, _addr) = listener.accept()?;
     let (ctl, mut reader) =
         rtlsdr_mt::open(args.device_index).map_err(|_| "Could not open RTL SDR device")?;
     let ctl = Arc::new(Mutex::new(ctl));
 
     let thread_ctl = std::thread::spawn({
-        let log = log.clone();
         let ctl = ctl.clone();
         let should_exit = should_exit.clone();
         let mut stream = stream.try_clone()?;
@@ -101,36 +97,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 match buf[0] {
                     0x01 => {
                         let freq = u32::from_be_bytes((&buf[1..5]).try_into().unwrap());
-                        info!(log, "setting center freq to {}", freq);
+                        info!("setting center freq to {}", freq);
                         ctl.lock().unwrap().set_center_freq(freq).unwrap();
                     }
                     0x02 => {
                         let sample_rate = u32::from_be_bytes((&buf[1..5]).try_into().unwrap());
-                        info!(log, "setting sample rate to {}", sample_rate);
+                        info!("setting sample rate to {}", sample_rate);
                         ctl.lock().unwrap().set_sample_rate(sample_rate).unwrap();
                     }
                     0x05 => {
                         let ppm = i32::from_be_bytes((&buf[1..5]).try_into().unwrap());
-                        info!(log, "setting ppm to {}", ppm);
+                        info!("setting ppm to {}", ppm);
                         ctl.lock().unwrap().set_ppm(ppm).unwrap();
                     }
                     0x04 => {
                         let gain = i32::from_be_bytes((&buf[1..5]).try_into().unwrap());
-                        info!(log, "setting manual gain to {}", gain);
+                        info!("setting manual gain to {}", gain);
                         ctl.lock().unwrap().set_tuner_gain(gain).unwrap();
                     }
                     0x08 => {
                         let agc = u32::from_be_bytes((&buf[1..5]).try_into().unwrap()) == 1u32;
                         if agc {
-                            info!(log, "setting automatic gain control to on");
+                            info!("setting automatic gain control to on");
                             ctl.lock().unwrap().enable_agc().unwrap();
                         } else {
-                            info!(log, "setting automatic gain control to off");
+                            info!("setting automatic gain control to off");
                             ctl.lock().unwrap().disable_agc().unwrap();
                         }
                     }
                     _ => {
-                        debug!(log, "recv unsupported command {:?}", buf);
+                        debug!("recv unsupported command {:?}", buf);
                     }
                 }
             }
@@ -140,7 +136,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let thread_cancel = std::thread::spawn({
         move || {
             receiver.recv().unwrap();
-            info!(log, "stopping read from device");
+            info!("stopping read from device");
             ctl.lock().unwrap().cancel_async_read();
             should_exit.store(true, Ordering::SeqCst);
         }
